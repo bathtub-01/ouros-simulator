6efbe3f239ec747ad9c132d78858970d8c834534
bathtub <yorkgank@gmail.com>
2025-02-28 16:08:36 +0000

    add: dual port memory

--- src/hardware/common/memory.rs
+++ src/hardware/common/memory.rs
@@ -2,19 +2,19 @@ use crate::hardware::common::Register;
 use crate::hw_module::{HwModule, HwStates};
 
 #[derive(Default)]
-pub struct MemInput<T: Clone + Default> {
+struct MemInput<T: Clone + Default> {
     is_write: bool,
     addr: usize,
     din: T,
 }
 
 #[derive(Default)]
-pub struct MemOutput<T: Clone + Default> {
+struct MemOutput<T: Clone + Default> {
     dout: T,
 }
 
 #[derive(Default)]
-pub struct SingleLocal<T: Clone + Default> {
+struct SingleLocal<T: Clone + Default> {
     ram: Vec<T>,
     holder: Register<T>,
 }
@@ -83,4 +83,140 @@ fn single_port_mem_spec() {
     }
 }
 
-pub struct DualPortMem {}
+#[derive(Default)]
+struct DualInput<T: Clone + Default> {
+    port_a: MemInput<T>,
+    port_b: MemInput<T>,
+}
+
+#[derive(Default)]
+struct DualOutput<T: Clone + Default> {
+    port_a: MemOutput<T>,
+    port_b: MemOutput<T>,
+}
+
+#[derive(Default)]
+struct DualLocal<T: Clone + Default> {
+    ram: Vec<T>,
+    holder_a: Register<T>,
+    holder_b: Register<T>,
+}
+
+/// Synchronous dual port read-write memory.
+/// Read-after-write for the same address.
+pub struct DualPortMem<T: Clone + Default> {
+    states: HwStates<DualInput<T>, DualLocal<T>, DualOutput<T>>,
+}
+
+impl<T: Clone + Default> DualPortMem<T> {
+    fn new(depth: usize) -> Self {
+        Self {
+            states: HwStates {
+                input: Default::default(),
+                local: DualLocal {
+                    ram: vec![T::default(); depth],
+                    holder_a: Default::default(),
+                    holder_b: Default::default(),
+                },
+                output: Default::default(),
+            },
+        }
+    }
+}
+
+impl<T: Clone + Default> HwModule for DualPortMem<T> {
+    fn update_local(&mut self) {
+        let input = &self.states.input;
+        let local = &mut self.states.local;
+
+        assert!(
+            !(input.port_a.is_write
+                && input.port_b.is_write
+                && input.port_a.addr == input.port_b.addr),
+            "DualPortMem: writing on the same addr is not allowed."
+        );
+
+        // A bit ugly, but read-after-write needs to be preserved...
+
+        if input.port_a.is_write {
+            local.ram[input.port_a.addr] = input.port_a.din.clone();
+        }
+
+        if input.port_b.is_write {
+            local.ram[input.port_b.addr] = input.port_b.din.clone();
+        }
+
+        if !input.port_a.is_write {
+            local.holder_a.connect(&local.ram[input.port_a.addr]);
+        }
+
+        if !input.port_b.is_write {
+            local.holder_b.connect(&local.ram[input.port_b.addr]);
+        }
+    }
+
+    fn tick_children(&mut self) {
+        self.states.local.holder_a.tick();
+        self.states.local.holder_b.tick();
+    }
+
+    fn gen_output(&mut self) {
+        self.states.output.port_a.dout = self.states.local.holder_a.value().clone();
+        self.states.output.port_b.dout = self.states.local.holder_b.value().clone();
+    }
+}
+
+#[test]
+fn dual_port_mem_spec() {
+    let mut mem: DualPortMem<u32> = DualPortMem::new(1024);
+
+    // Common read/write
+    for i in 50..100 {
+        mem.states.link_input(|input| {
+            input.port_a.addr = i;
+            input.port_a.is_write = true;
+            input.port_a.din = i as u32 + 100;
+            input.port_b.addr = i + 200;
+            input.port_b.is_write = true;
+            input.port_b.din = i as u32 + 100;
+        });
+        mem.tick();
+    }
+
+    for i in 50..100 {
+        mem.states.link_input(|input| {
+            input.port_a.addr = i;
+            input.port_a.is_write = false;
+            input.port_b.addr = i + 200;
+            input.port_b.is_write = false;
+        });
+        mem.tick();
+        assert_eq!(mem.states.output.port_a.dout, i as u32 + 100);
+        assert_eq!(mem.states.output.port_b.dout, i as u32 + 100);
+    }
+
+    // Read-after-write
+    for i in 500..600 {
+        mem.states.link_input(|input| {
+            input.port_a.addr = i;
+            input.port_a.is_write = true;
+            input.port_a.din = i as u32 + 100;
+            input.port_b.addr = i;
+            input.port_b.is_write = false;
+        });
+        mem.tick();
+        assert_eq!(mem.states.output.port_b.dout, i as u32 + 100);
+    }
+
+    for i in 500..600 {
+        mem.states.link_input(|input| {
+            input.port_b.addr = i;
+            input.port_b.is_write = true;
+            input.port_b.din = i as u32 - 100;
+            input.port_a.addr = i;
+            input.port_a.is_write = false;
+        });
+        mem.tick();
+        assert_eq!(mem.states.output.port_a.dout, i as u32 - 100);
+    }
+}
